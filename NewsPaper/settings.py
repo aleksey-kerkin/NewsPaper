f4053efd31ac0cb9a670921fa37301dd4b24239e
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-04-23 23:21:38 +0500

    Add logging

--- NewsPaper/settings.py
+++ NewsPaper/settings.py
@@ -1,7 +1,9 @@
 import os
+import logging
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 
+logger = logging.getLogger("django")
 load_dotenv(find_dotenv())
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,4 +142,116 @@ CACHES = {
         "BACKEND": "django.core.cache.backends.filebased.FileBasedCache",
         "LOCATION": BASE_DIR / "cache_files",
     }
+}
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "style": "{",
+    "formatters": {
+        "simple": {
+            "format": "%(asctime)s %(levelname)s %(message)s",
+            "datefmt": "%d.%m.%Y %H-%M-%S",
+        },
+        "simple_warning": {
+            "format": "%(asctime)s %(levelname)s %(message)s %(pathname)s"
+        },
+        "simple_error": {
+            "format": "%(asctime)s %(levelname)s %(message)s %(exc_info)s"
+        },
+        "general": {
+            "format": "%(asctime)s %(levelname)s %(module)s %(message)s",
+            "datefmt": "%d.%m.%Y %H-%M-%S",
+        },
+        "errors": {
+            "format": "%(asctime)s %(levelname)s %(pathname)s %(message)s %(exc_info)s",
+            "datefmt": "%d.%m.%Y %H-%M-%S",
+        },
+        "email": {
+            "format": "%(asctime)s %(levelname)s %(message)s %(pathname)s",
+            "datefmt": "%d.%m.%Y %H-%M-%S",
+        },
+        "security": {
+            "format": "%(asctime)s %(levelname)s %(module)s %(message)s",
+            "datefmt": "%d.%m.%Y %H-%M-%S",
+        },
+    },
+    "filters": {
+        "require_debug_true": {
+            "()": "django.utils.log.RequireDebugTrue",
+        },
+        "require_debug_false": {
+            "()": "django.utils.log.RequireDebugFalse",
+        },
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+        "console_warning": {
+            "level": "WARNING",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+            "formatter": "simple_warning",
+        },
+        "console_error": {
+            "level": "ERROR",
+            "filters": ["require_debug_true"],
+            "class": "logging.StreamHandler",
+            "formatter": "simple_error",
+        },
+        "general": {
+            "level": "INFO",
+            "filters": ["require_debug_false"],
+            "class": "logging.FileHandler",
+            "filename": "logs/general.log",
+            "formatter": "general",
+        },
+        "errors": {
+            "level": "ERROR",
+            "class": "logging.FileHandler",
+            "filename": "logs/errors.log",
+            "formatter": "errors",
+        },
+        "security": {
+            "level": "INFO",
+            "class": "logging.FileHandler",
+            "filename": "logs/security.log",
+            "formatter": "security",
+        },
+        "mail_admins": {
+            "level": "ERROR",
+            "class": "django.utils.log.AdminEmailHandler",
+            "formatter": "email",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console", "console_warning", "console_error", "general"],
+            "propagate": True,
+        },
+        "django.request": {
+            "handlers": ["errors", "mail_admins"],
+            "propagate": True,
+        },
+        "django.server": {
+            "handlers": ["errors", "mail_admins"],
+            "propagate": True,
+        },
+        "django.template": {
+            "handlers": ["errors"],
+            "propagate": True,
+        },
+        "django.db_backends": {
+            "handlers": ["errors"],
+            "propagate": True,
+        },
+        "django.security": {
+            "handlers": ["security"],
+            "propagate": True,
+        },
+    },
 }
